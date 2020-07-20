9d58dd11b6f6db3fe993124dadefe7982821a4d1
Douglas Goddard <douglas.gastonguay@gmail.com>
2020-07-19 21:16:00 -0700

    Add unused randbyte strategy.

--- strategies.go
+++ strategies.go
@@ -18,6 +18,54 @@ func NewStrats() map[string]Mutator {
 	return strats
 }
 
+type ByteDelta struct {
+	Offset uint64
+	Value  uint8
+}
+
+type RandByte struct {
+	Diffs map[string][]ByteDelta
+}
+
+func (rb *RandByte) Generate(data []byte, mapping map[int]int, name string) error {
+	if len(data) < 1 {
+		return errors.New("zero size data")
+	}
+
+	rb.Diffs[name] = make([]ByteDelta, 0)
+
+	count := 1 + int(rand.Uint64()%8)
+	for i := 0; i < count; i++ {
+		rb.Diffs[name] = append(rb.Diffs[name], ByteDelta{
+			Offset: rand.Uint64() % uint64(len(data)),
+			Value:  uint8(rand.Uint32() & 0xFF),
+		})
+	}
+
+	return nil
+}
+
+func (rb *RandByte) Synthesize(data []byte, name string) []byte {
+	deltas, ok := rb.Diffs[name]
+	if !ok {
+		return data
+	}
+
+	sz := len(data)
+	bs := make([]byte, sz, sz)
+	copy(bs, data)
+
+	for _, bd := range deltas {
+		bs[bd.Offset] = bd.Value
+	}
+
+	return bs
+}
+
+func (rb *RandByte) Remove(name string) {
+	delete(rb.Diffs, name)
+}
+
 type BitFlip struct {
 	Flips map[string]uint64
 }
